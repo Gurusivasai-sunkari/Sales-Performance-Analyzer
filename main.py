e216b2b524d4c226b90503649598b08781a1d4d9
Gurusivasai-sunkari <gurusivasai09@gmail.com>
2026-06-02 17:27:33 +0530

    Add monthly sales input and analysis functionality

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import numpy as np
+from src.analysis import analyze_sales
 
+months = np.array([
+    "Jan", "Feb", "Mar", "Apr",
+    "May", "Jun", "Jul", "Aug",
+    "Sep", "Oct", "Nov", "Dec"
+])
+
+sales = []
+
+print("Enter monthly sales in $:")
+
+for month in months:
+    value = float(input(f"{month}: "))
+    sales.append(value)
+
+sales = np.array(sales)
+
+analyze_sales(sales, months)
